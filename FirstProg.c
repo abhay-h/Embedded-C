22550a09aa351be31d5aa81682867d4878ce7e89
abhay-h <abhaytaras18@gmail.com>
2021-12-05 15:52:54 +0530

    pop

--- FirstProg.c
+++ FirstProg.c
@@ -3,6 +3,16 @@
 
 int main()
 {
-   printf("hello");
+   int Up_Time;
+   char Set_Time_name[100];
+   printf("hello\t\n");
+
+   gets(Set_Time_name);
+
+   scanf("%d",&Up_Time);
+
+   printf("%d%s",Up_Time, Set_Time_name);
+
+
    return 0;
 }
