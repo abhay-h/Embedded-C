544206a7e5fd74ee3e6e28966594b27dd48e0041
abhay-h <abhayyt17@gmail.com>
2022-01-13 22:58:09 +0530

    testing

--- FirstProg.c
+++ FirstProg.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc , char *argv[])
 {
    int Up_Time;
    char Set_Time_name[100];
    printf("hello\t\n");
 
-   gets(Set_Time_name);
+   scanf("%[^\n]s",Set_Time_name);
 
    scanf("%d",&Up_Time);
 
-   printf("%d%s",Up_Time, Set_Time_name);
+   printf("%d\n%s",Up_Time, Set_Time_name);
+
 
 
    return 0;
